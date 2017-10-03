fa455816f22b7b1f498b45184b7631e2c1ba7389
Jihane <jihane.bouhammada@Jihanes-MacBook-Air.local>
2017-10-03 06:54:57 +0200

    finished

--- kernels.py
+++ kernels.py
@@ -1,2 +1,25 @@
-import numpy
+# All kernels should go here
+import numpy, math
 
+def linearKernel(vec1, vec2):
+    z   = numpy.dot(vec1, vec2) + 1
+    return(z)
+
+def polyKernel(vec1, vec2, p):
+    z   = numpy.dot(vec1, vec2) + 1
+    return(numpy.power(z, p))
+
+def radialKernel(vec1, vec2, sigma):
+    diff = vec1 - vec2
+    return(numpy.exp(-numpy.dot(diff,diff)/(2*sigma**2)) )
+
+def sigmoidKernel(vec1, vec2, k, delta):
+    return(numpy.tanh(numpy.dot(k*vec1,vec2) - delta))
+
+def main():
+    testArray   = numpy.array([1,2,3,4])
+    out         = polyKernel(testArray, testArray,2)
+    print(out)
+
+if __name__ == '__main__':
+    main()
